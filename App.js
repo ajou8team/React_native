8d3c492ae877432813b08db654314cd901097713
haejin <bae05030@gmail.com>
2019-07-25 15:17:57 +0900

    Custom component

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { StyleSheet, Button, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends React.Component{
+  render(){
+    return(
+      <View style = {styles.container}>
+        <CounterButton/>
+      </View>
+    );
+  }
+
+}
+class CounterButton extends React.Component {
   state = {
     counter : 0,
   };
@@ -12,7 +22,7 @@ export default class App extends React.Component {
   };
   render(){
     return (
-      <View style={styles.container}>
+      <View>
         <Text>{this.state.counter}</Text>
          <Button title={'click me!'} onPress={this.clickHandler}/>
       </View>
