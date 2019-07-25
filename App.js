d7b16aa428b8a6e68ba343029e8dfc9ab60c8465
HongJow <hoongjow@gmail.com>
2019-07-26 01:49:48 +0900

    overflowed contents

--- App.js
+++ App.js
@@ -1,50 +1,26 @@
 import React from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { Constants } from 'expo';
 
-// class CounterButton extends React.Component {
-//   static defaultProps ={
-//     counter : -1,
-//   };
- 
-//   constructor(props){
-//     super(props);
-//     this.state = {
-//       counter: props.counter,
-//     }
-//   }
-//   clickHandler = () => {
-//     this.setState({
-//       counter: this.state.counter +1,
-//     });
-//   };
-//   render(){
-//     return (
-//       <View style={styles.buttonContainer}>
-//         <Text>{this.state.counter}</Text>
-//          <Button title={'click me!'} onPress={this.clickHandler}/>
-//       </View>
-//     );
-//   }
-// }
-
-export default class App extends React.Component{
-  render(){
-    return(
-      <View style = {styles.container}>
-        {/* <CounterButton counter = {0}/>
-        <CounterButton count ={1} />
-        <CounterButton counter = {1}/>
-        <CounterButton counter = {2}/>
-        <CounterButton counter = {3}/>
-        <Text style={styles.text}>1</Text>
-        <Text style={[styles.text, styles.text2]}>2</Text>
-        <Text style={styles.text}>3</Text> */}
-        <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, }}/>
-      </View>
+export default class App extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Text style={{fontSize:96}}>Scroll me plz</Text>
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+          <Image source={{uri: "https://facebook.github.io/react-native/img/favicon.png", width: 64, height: 64}} />
+        </View>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
@@ -52,28 +28,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     marginTop: Constants.statusBarHeight,
-    
+
     borderColor: 'red',
     borderWidth: 2,
-    // justifyContent: 'center',
-    // alignItems: 'stretch',
   },
-
-  // text: {
-  //   flex: 1,
-  //   borderWidth: 1,
-  //   borderColor: 'red',
-
-  //   textAlign: 'center',
-  //   textAlignVertical: 'center',
-  //   fontSize: 24,
-  //   fontWeight: 'bold',
-  // },
-
-  // text2: {
-  //   flex: 2,
-  // }
-  // buttonContainer:{
-  //   width: '100%',
-  // },
 });
